df3df1c369bdb9289a0463c0bac10287c177dabe
SGrinstead <seth.grinstead3299@gmail.com>
2023-08-02 13:16:53 -0700

    Show Test

--- CoffeeShopTests/OrdersControllerTests.cs
+++ CoffeeShopTests/OrdersControllerTests.cs
@@ -54,5 +54,37 @@ namespace CoffeeShopTests
 			Assert.Contains($"Order {order2.Id}", html);
 		}
 
+		[Fact]
+		public async Task Show_DisplaysViewOfSingleCustomerOrder()
+		{
+			var context = GetDbContext();
+			var client = _factory.CreateClient();
+
+			Customer customer = new Customer { Name = "Seth", Email = "seth@aol.com" };
+			Order order1 = new Order { DateCreated = DateTime.Now.ToUniversalTime() };
+			Order order2 = new Order { DateCreated = DateTime.Now.ToUniversalTime() };
+			Item item1 = new Item { Name = "Chai Latte", PriceInCents = 450 };
+			Item item2 = new Item { Name = "Scone", PriceInCents = 300 };
+			Item item3 = new Item { Name = "Coffee", PriceInCents = 325 };
+
+			order1.Items.Add(item1);
+			order1.Items.Add(item2);
+			order2.Items.Add(item3);
+			customer.Orders.Add(order1);
+			customer.Orders.Add(order2);
+
+			context.Customers.Add(customer);
+			context.SaveChanges();
+
+			var response = await client.GetAsync($"/customers/{customer.Id}/orders/{order1.Id}");
+			var html = await response.Content.ReadAsStringAsync();
+
+			response.EnsureSuccessStatusCode();
+			Assert.Contains("Chai Latte", html);
+			Assert.Contains("Scone", html);
+			Assert.Contains("1 Chai", html);
+			Assert.Contains("2 Scone", html);
+			Assert.Contains($"Order {order1.Id}", html);
+		}
 	}
 }
